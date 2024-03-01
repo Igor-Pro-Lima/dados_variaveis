695d037151158298c862ecf94dca9b1d29bbf207
Igor Lima <igorlimaproenca@gmail.com>
2024-03-01 02:00:25 -0300

    Adicionar arquivos de projeto.

---
+++ dados_variaveis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dados_variaveis {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int idade;
+            double salario, altura;
+            char genero;
+            string nome;
+
+            idade = 32;
+            salario = 4000.9;
+            altura = 1.75;
+            genero = 'M';
+            nome = "Igor Lima";
+
+            Console.WriteLine(idade);
+            Console.WriteLine(salario);
+            Console.WriteLine(altura);  
+            Console.WriteLine(genero);
+            Console.WriteLine(nome);
+
+        }
+    }
+}
